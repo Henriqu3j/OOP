c50cd9fb9d146735a2f951a28f5a1aa0dc20bb2c
Henriqu3j <hjoviniano@gmail.com>
2022-11-06 17:30:51 -0300

    reading the product name and price

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,48 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.OrderItem;
-import entities.Product;
+import entities.Client;
+import entities.enums.OrderStatus;
 
 public class Program {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		Product p = new Product("TV", 1000.0);
+		System.out.println("Enter client data: ");
+		System.out.println("Name: ");
+		String name = sc.nextLine();
+		System.out.println("Email: ");
+		String email = sc.next();
+		System.out.println("Birth date (DD/MM/YYYY: ");
+		Date birthDate = sdf.parse(sc.next());
 		
-		OrderItem orderItem1 = new OrderItem(1, 1000.0, p);
+		Client client = new Client(name, email, birthDate);
 		
-		System.out.println(orderItem1);
+		System.out.println("Enter order data: ");
+		System.out.println("Status: ");
+		OrderStatus status = OrderStatus.valueOf(sc.next());
+		
+		System.out.println("How many items to this order? ");
+		int N = sc.nextInt();
+		for (int i=1; i<N; i++) {
+			System.out.println("Enter #1" + i + " item data: ");
+		}
+		
+		sc.close();
+	
+		
+		Client cli = new Client("Maria", "maria@gmail.com", sdf.parse("20/10/1995"));
+		
+		System.out.println(cli);
 		
 		sc.close();
 	}
